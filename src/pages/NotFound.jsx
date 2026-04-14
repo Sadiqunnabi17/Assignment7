99549d9494a5e54c2fd5336b0287d9c7b1e95268
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-04-14 18:54:43 +0600

    Added 404 Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
 const NotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="min-h-[60vh] flex flex-col items-center justify-center text-center">
+
+      <h1 className="text-4xl font-bold text-red-500">404</h1>
+
+      <p className="mt-4 text-2xl font-bold text-blue-600">
+        Page not found !!
+      </p>
+
+    </div>
+  );
 };
 
 export default NotFound;
